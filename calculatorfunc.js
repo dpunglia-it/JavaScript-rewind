11753e478e9dfcdce9eb5b52e427f2b4aa2677c3
dpunglia-it <dpunglia@trueigtech.com>
2026-01-14 15:05:42 +0530

    calculator functions

---
+++ calculatorfunc.js
@@ -0,0 +1,15 @@
+function add(a,b) {
+    return a+b ;
+} 
+function substract(a,b) {
+    return a-b;
+}
+function multiplication(a,b){
+    return a*b;
+}
+function division(a,b){
+    return a/b;
+}
+
+const addition=add(3,4);
+console.log(addition);
